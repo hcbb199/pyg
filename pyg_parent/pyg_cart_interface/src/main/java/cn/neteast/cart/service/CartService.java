39268c252363825f60ee587ed93ae84543a34f94
Amber <huchen94@163.com>
2019-09-15 08:05:32 +0800

    day18更新

---
+++ pyg_parent/pyg_cart_interface/src/main/java/cn/neteast/cart/service/CartService.java
@@ -0,0 +1,20 @@
+package cn.neteast.cart.service;
+
+import cn.neteast.pojogroup.Cart;
+
+import java.util.List;
+
+/**
+ * 购物车服务接口
+ */
+public interface CartService {
+    /**
+     * 添加商品到购物车
+     *
+     * @param cartList
+     * @param itemId
+     * @param num
+     * @return
+     */
+    List<Cart> addGoodsToCartList(List<Cart> cartList, Long itemId, Integer num);
+}
