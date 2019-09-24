8ecf33d6b53f9f185ed97fa77ce5c6eb7eb43abb
Amber <huchen94@163.com>
2019-09-24 08:08:21 +0800

    最终提交

--- pyg_parent/pyg_task_service/src/main/java/cn/neteast/task/SeckillTask.java
+++ pyg_parent/pyg_task_service/src/main/java/cn/neteast/task/SeckillTask.java
@@ -25,41 +25,48 @@ public class SeckillTask {
      */
     @Scheduled(cron = "* * * * * ?")
     public void refreshSeckillGoods() {
-        //System.out.println("执行了任务调度" + new Date());
+        System.out.println("执行了任务调度" + new Date());
         //查询所有的秒杀商品键集合
-        List ids = new ArrayList( redisTemplate.boundHashOps("seckillGoods").keys());
+        List<Long> ids = new ArrayList<Long>(redisTemplate.boundHashOps("seckillGoods").keys());
+
         //查询正在秒杀的商品列表
-        TbSeckillGoodsExample example=new TbSeckillGoodsExample();
+        TbSeckillGoodsExample example = new TbSeckillGoodsExample();
         TbSeckillGoodsExample.Criteria criteria = example.createCriteria();
         criteria.andStatusEqualTo("1");//审核通过
         criteria.andStockCountGreaterThan(0);//剩余库存大于0
         criteria.andStartTimeLessThanOrEqualTo(new Date());//开始时间小于等于当前时间
         criteria.andEndTimeGreaterThan(new Date());//结束时间大于当前时间
-        criteria.andIdNotIn(ids);//排除缓存中已经有的商品
-        List<TbSeckillGoods> seckillGoodsList= seckillGoodsMapper.selectByExample(example);
-        //装入缓存
-        for( TbSeckillGoods seckill:seckillGoodsList ){
-            redisTemplate.boundHashOps("seckillGoods").put(seckill.getId(), seckill);
+        if (ids.size() > 0) {
+            criteria.andIdNotIn(ids);//排除缓存中已经有的商品
         }
-        System.out.println("将"+seckillGoodsList.size()+"条商品装入缓存");
+        List<TbSeckillGoods> seckillGoodsList = seckillGoodsMapper.selectByExample(example);
+        if (seckillGoodsList != null && seckillGoodsList.size() > 0) {
+            //装入缓存
+            for (TbSeckillGoods seckill : seckillGoodsList) {
+                redisTemplate.boundHashOps("seckillGoods").put(seckill.getId(), seckill);
+
+            }
+        }
+
+        //System.out.println("将" + seckillGoodsList.size() + "条商品装入缓存");
     }
 
     /**
      * 移除秒杀商品
      */
-    @Scheduled(cron="* * * * * ?")
-    public void removeSeckillGoods(){
+    @Scheduled(cron = "* * * * * ?")
+    public void removeSeckillGoods() {
         System.out.println("移除秒杀商品任务在执行");
-        //扫描缓存中秒杀商品列表，发现过期的移除
+        //扫描缓存中秒杀商品列表, 发现过期的移除
         List<TbSeckillGoods> seckillGoodsList = redisTemplate.boundHashOps("seckillGoods").values();
-        for( TbSeckillGoods seckill:seckillGoodsList ){
-            if(seckill.getEndTime().getTime()<new Date().getTime()  ){//如果结束日期小于当前日期，则表示过期
+        for (TbSeckillGoods seckill : seckillGoodsList) {
+            if (seckill.getEndTime().getTime() < new Date().getTime()) {//如果结束日期小于当前日期，则表示过期
                 seckillGoodsMapper.updateByPrimaryKey(seckill);//向数据库保存记录
                 redisTemplate.boundHashOps("seckillGoods").delete(seckill.getId());//移除缓存数据
-                System.out.println("移除秒杀商品"+seckill.getId());
+                //System.out.println("移除秒杀商品" + seckill.getId());
             }
         }
-        System.out.println("移除秒杀商品任务结束");
+        //System.out.println("移除秒杀商品任务结束");
     }
 
 }
